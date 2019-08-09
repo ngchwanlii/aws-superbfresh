8ac9496c0abf380cbbb8e8c22298953ecf2308ea
ngchwanlii <ngchwanlii@gmail.com>
2019-08-09 13:25:40 -0700

    Added LoginRequired and CSRFExempt mixin

---
+++ utils/mixin.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+
+
+class LoginRequiredMixin(object):
+    @classmethod
+    def as_view(cls, **kwargs):
+        view = super(LoginRequiredMixin, cls).as_view(**kwargs)
+        return login_required(view)
+
+
+class CSRFExemptMixin(object):
+    @method_decorator(csrf_exempt)
+    def dispatch(self, *args, **kwargs):
+        return super(CSRFExemptMixin, self).dispatch(*args, **kwargs)
