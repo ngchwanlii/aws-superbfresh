bd4d40f3a0f977fbc9f6d40ef2b1de50f2b866fa
ngchwanlii <ngchwanlii@gmail.com>
2019-08-16 08:58:08 -0700

    Removed PORT in activate url

--- celery_tasks/tasks.py
+++ celery_tasks/tasks.py
@@ -6,11 +6,13 @@ from email.mime.text import MIMEText
 import django
 from celery import Celery
 from django.conf import settings
+from django.template import loader
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'superbfresh.settings')
 django.setup()
 
 # import apps' model should be written after django.setup()!
+from apps.goods.models import GoodsType, IndexGoodsBanner, IndexPromotionBanner, IndexTypeGoodsBanner
 
 # Celery
 app = Celery('celery_tasks.tasks', broker=settings.REDIS_BROKER_LOCATION)
@@ -18,10 +20,9 @@ app = Celery('celery_tasks.tasks', broker=settings.REDIS_BROKER_LOCATION)
 
 @app.task
 def send_register_active_email(to_email, username, token):
-    activate_url = "{SCHEME}://{HOST}:{PORT}/user/active/{token}".format(SCHEME=settings.BASE_SCHEME,
-                                                                         HOST=settings.BASE_HOST,
-                                                                         PORT=settings.BASE_PORT,
-                                                                         token=token)
+    activate_url = "{SCHEME}://{HOST}/user/active/{token}".format(SCHEME=settings.BASE_SCHEME,
+                                                                  HOST=settings.BASE_HOST,
+                                                                  token=token)
 
     html_message = """\
     <html>
@@ -45,3 +46,35 @@ def send_register_active_email(to_email, username, token):
         s.login(settings.AWS_SES_ACCESS_KEY_ID, settings.AWS_SES_SECRET_ACCESS_KEY)
         s.send_message(msg)
         s.quit()
+
+
+@app.task
+def generate_static_index_html():
+    # goods types
+    types = GoodsType.objects.all()
+
+    # goods home page banners
+    goods_banners = IndexGoodsBanner.objects.all().order_by('index')
+
+    promotion_banners = IndexPromotionBanner.objects.all().order_by('index')
+
+    # obtain goods' types=categories image banners / title tag
+    for type in types:  # GoodsType
+        image_banners = IndexTypeGoodsBanner.objects.filter(type=type, display_type=1).order_by('index')
+        title_banners = IndexTypeGoodsBanner.objects.filter(type=type, display_type=0).order_by('index')
+        type.image_banners = image_banners
+        type.title_banners = title_banners
+
+    context = {'types': types,
+               'goods_banners': goods_banners,
+               'promotion_banners': promotion_banners,
+               'cart_count': 0,
+               }
+
+    template = loader.get_template('static_index.html')
+    static_index_html = template.render(context)
+
+    # generate static index html
+    index_html_path = os.path.join(settings.BASE_DIR, 'static/index.html')
+    with open(index_html_path, 'w') as f:
+        f.write(static_index_html)
